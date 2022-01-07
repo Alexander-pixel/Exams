6fa513740a30d47ee20ad397aa2e9f1cb0641e25
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-07 14:27:12 +0200

    Update section.js

--- Tasks/section.js
+++ Tasks/section.js
@@ -1,15 +1,20 @@
 // Split string by the first occurrence of separator
 
-SectionString   = (s, t ) => {  ; 
-i = s.indexOf(t) 
-if(i < 0){ 
-return[s, ''] 
-} 
-else 
-{ 
-return[s.slice(0,i),s.slice(i+t.length)] 
-} 
-} 
+'use strict';
 
-const result = SectionString('All you need is JavaScript', 'is')
-console.log(result)
+const splitString = (str, separator) => {
+  const index = str.indexOf(separator);
+  const res = [];
+  if (index === -1) {
+    return res.push(str);
+  } else {
+    res.push(str.slice(0, index));
+    res.push(str.slice(index + separator.length, str.length));
+    return res;
+  }
+};
+
+// Usage
+
+const result = splitString('All you need is JavaScript', 'is');
+console.log(result);
