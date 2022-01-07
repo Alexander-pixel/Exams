a60c9d166f71f4117bd8b15f04f049647a19d4cc
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-07 14:18:43 +0200

    Update difference.js

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -3,14 +3,14 @@
 'use strict';
 
 const difference = (obj1, obj2) => {
-    const res = {};
-    for (const key in obj1) {
-        if (!(key in obj2)) {
-            const value = obj1[key];
-            res[key] = value;
-        }
+  const res = {};
+  for (const key in obj1) {
+    if (!(key in obj2)) {
+      const value = obj1[key];
+      res[key] = value;
     }
-    return res;
+  }
+  return res;
 };
 
 // Usage
