140651353d48ea95921e416c93a442102a6bfbee
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-07 14:18:17 +0200

    Update count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,14 +3,14 @@
 'use strict';
 
 const count = obj => {
-    let sum = 0;
-    for (const key in obj) {
-        const value = obj[key];
-        if (typeof value === 'number') {
-            sum += value;
-        }
+  let sum = 0;
+  for (const key in obj) {
+    const value = obj[key];
+    if (typeof value === 'number') {
+      sum += value;
     }
-    return sum;
+  }
+  return sum;
 };
 
 // Usage
