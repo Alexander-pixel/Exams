e06b0aa8b4bce74675cfe56ef6f2559ff9301e10
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-07 14:26:31 +0200

    Update reverse.js

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,12 +1,18 @@
 // Reverse dict, exchange key and value
 
-Reverse =DATA=> {
- T = Object.keys(DATA, 500);({...DATA});
-   T.forEach((_) => {
-      const v1 = DATA[_];
-      DATA[v1]= _; delete DATA[_];
-   }, 1000)
-return DATA};
+'use strict';
 
-const result = Reverse({ a: 'uno', b: 'due', c: 'tre' });
+const reverse = dict => {
+  const keys = Object.keys(dict);
+  for (const key of keys) {
+    const value = dict[key];
+    dict[value] = key;
+    delete dict[key];
+  }
+  return dict;
+};
+
+// Usage
+
+const result = reverse({ a: 'uno', b: 'due', c: 'tre' });
 console.log(result);
