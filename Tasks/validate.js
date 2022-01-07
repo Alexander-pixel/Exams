4398effe1aa32734132add3d15071f7cfeed21eb
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-07 14:30:10 +0200

    Update validate.js

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,18 +1,14 @@
 // Validate person name
 
-const isValidate = (T) => {
-if (!T) return false;
-if (T === '') return false;
-if (typeof T !== 'string') return false;
-if (T.length === 0) return false;
-if (!T.includes(' ')) return false; {
-  for (C of T) {
-    if (C === ' ') continue;
-    if (C.toLowerCase().charCodeAt(0) >= 97 && C.toLowerCase().charCodeAt(0) <= 122) { } else { return false; }
-  };
-  return true;
-};
+'use strict';
+
+const isValid = name => {
+  if (typeof name !== 'string') return false;
+  const regex = /^[a-zA-Z]+ [a-zA-Z]+$/;
+  return regex.test(name);
 };
 
-const result = isValidate('Marcus Aurelius');
+// Usage
+
+const result = isValid('Marcus Aurelius');
 console.log(result);
