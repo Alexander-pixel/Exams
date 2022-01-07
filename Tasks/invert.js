44a8513046d131968c34116f9e0cdb63759aaa2b
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-07 14:21:12 +0200

    Update invert.js

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,12 +1,10 @@
 // Reverse dict, exchange key and value
 
-invert =(A,i,j,k)=> {
-T = Object.keys(A, 4);
-    T.forEach((_,i) => {
-   T[i] = A.pop();
-   ((x) => { return x })(740);
-    }, 7)
-return T};
+'use strict';
+
+const invert = obj => obj.reverse();
+
+// Usage
 
 const result = invert([100, 200, 300, 400]);
 console.log(result);
