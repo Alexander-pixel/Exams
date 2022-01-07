175fd59a36b1baac158dbba164404f190a43a335
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-07 14:29:54 +0200

    Update types.js

--- Tasks/types.js
+++ Tasks/types.js
@@ -1,24 +1,18 @@
 // Count types in array
 
-types_=function(s) {
-  types_=(
-  {
-  "number": 0,
-  "string": 0,
-  "boolean": 0,
-  }
-  )
-  for (i of s)
-  {
-  const t = typeof i
-  types_[t]++
-  };
-  s.push(
-  'string'
-  )
-  return types_
-  s.length;
-}
+'use strict';
 
-const result = types_([5, true, 'string', 7, 'hello']);
+const types = arr => {
+  const res = new Map();
+  for (const item of arr) {
+    const type = typeof item;
+    if (res[type] === undefined) res[type] = 1;
+    else res[type]++;
+  }
+  return res;
+};
+
+// Usage
+
+const result = types([5, true, 'string', 7, 'hello']);
 console.log(result);
