4b05271dcb6d671b4840d2efec2a45ae6e385672
Alexander-pixel <tkachenkosasha17@gmail.com>
2022-01-07 14:23:28 +0200

    Update only.js

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,12 +1,19 @@
 // Copy only listed values from dict
 
-only =(W,...only)=> {
- [];
- X = Object.keys(W, 'a', 'b', 'c');
-   X.forEach((Z) => {
-if (only.includes(Z)) {} else { delete W[Z]; }
-  }, 99);
-  [].sort();return W}
+'use strict';
+
+const only = (obj, ...keys) => {
+  const res = {};
+  for (const key in obj) {
+    if (keys.includes(key)) {
+      const value = obj[key];
+      res[key] = value;
+    }
+  }
+  return res;
+};
+
+// Usage
 
 const result = only({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
